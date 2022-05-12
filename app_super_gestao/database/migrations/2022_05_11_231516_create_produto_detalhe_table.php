6a5c107607eda0074ccfbbe3909059c9438d1764
Matheus Melo <matheus@santins.com.br>
2022-05-12 19:36:35 -0300

    initial

---
+++ app_super_gestao/database/migrations/2022_05_11_231516_create_produto_detalhe_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutoDetalheTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produto_detalhe', function (Blueprint $table) {
+            //colunas
+            $table->id();
+            $table -> unsignedBigInteger('produto_id');
+            $table -> float('comprimento', 8, 2);
+            $table -> float('largura', 8, 2);
+            $table -> float('altura', 8, 2);
+
+            $table->timestamps();
+
+            //referenciando o id da tabela produtos com o produto_id desta tabela
+            $table -> foreign('produto_id') -> references('id')-> on('produtos');
+            $table -> unique('produto_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produto_detalhe');
+    }
+}
